60007aeca914d45875945d74f5df8f1b03985428
Said Elsoudy <elsoudy222@gmail.com>
2023-05-09 15:17:19 +0300

    update child profile

--- lib/view/child_profile/child_profile_screen.dart
+++ lib/view/child_profile/child_profile_screen.dart
@@ -83,17 +83,6 @@ class ChildProfileScreen extends StatelessWidget {
                       height: 12,
                     ),
                     CustomTransaction(
-                      transactionTitle: "GTA IV Game",
-                      transactionAmount: "300",
-                      transactionDate: "May 25,2022 ",
-                      transactionTime: "10:00 pm",
-                      iconData: IconlyLight.arrow_down,
-                      iconColor: Colors.red,
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    CustomTransaction(
                       transactionTitle: " Football online",
                       transactionAmount: "300",
                       transactionDate: "May 25,2022 ",
